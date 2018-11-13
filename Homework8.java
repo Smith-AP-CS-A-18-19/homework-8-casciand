2d8003cf4bf4651b53abb2c126d0944b508b7d6c
casciand <casciand@berkeleyprep.org>
2018-11-12 20:25:54 -0500

    final commit

--- Homework8.java
+++ Homework8.java
@@ -42,27 +42,98 @@ public class Homework8 {
 	/* Find and return the maximum value in the
 	 * provided array
 	 */
-	public int problem4(double[] arr) {
+	public double problem4(double[] arr) {
 			double maximum = arr[0];
 			for (int i = 1; i < arr.length; i++) {
 					if (arr[i] > maximum) {
 							maximum = arr[i];
 					}
 			}
-			return (int) maximum;
+			return maximum;
 	}
 
 	/* Find and return the index of num in the
 	 * provided array
 	 */
 	public int problem5(int[] arr, int num) {
-			int index = 0;
+			int index = -1;
 			for (int i = 0; i < arr.length; i++) {
 					if (arr[i] == num) {
-							index = i + 1;
+							index = i;
 					}
 			}
 			return index;
 	}
 
+	public static void main(String[] args) {
+	                                boolean pass = true;
+
+	                                Homework8 hw8 = new Homework8(11);
+
+	                                int[] arr = hw8.problem1();
+	                                if (arr.length == 11) {
+	                                                System.out.println("Pass 1");
+	                                } else {
+	                                                System.out.println("Fail 1");
+	                                                pass = false;
+	                                }
+
+	                                hw8.problem2(7);
+	                                boolean pass2 = true;
+	                                for (int n : arr) {
+	                                                if (n != 7) {
+	                                                                pass2 = false;
+	                                                }
+	                                }
+	                                if (pass2) {
+	                                                System.out.println("Pass 2");
+	                                } else {
+	                                                System.out.println("Fail 2");
+	                                                pass = false;
+	                                }
+
+	                                boolean pass3 = true;
+	                                int value = 13;
+	                                hw8.problem3(value);
+	                                for (int i = 0; i < arr.length; i++) {
+	                                                if (arr[i] != value) {
+	                                                                pass3 = false;
+	                                                }
+	                                                value--;
+	                                }
+	                                if (pass3) {
+	                                                System.out.println("Pass 3");
+	                                } else {
+	                                                System.out.println("Fail 3");
+	                                                pass = false;
+	                                }
+
+	                                double[] arr2 = {2.3, 3.5, 5.7, 7.11, 5.13, 3.17, 2.19};
+	                                if (hw8.problem4(arr2) == 7.11) {
+	                                                System.out.println("Pass 4");
+	                                } else {
+	                                                System.out.println("Fail 4");
+	                                                pass = false;
+	                                }
+
+	                                int[] arr3 = {2, 3, 5, 7, 11, 13};
+	                                if (hw8.problem5(arr3, 5) == 2) {
+	                                                System.out.println("Pass 5.1");
+	                                } else {
+	                                                System.out.println("Fail 5.1");
+	                                                pass = false;
+	                                }
+	                                if (hw8.problem5(arr3, 9) == -1) {
+	                                                System.out.println("Pass 5.2");
+	                                } else {
+	                                                System.out.println("Fail 5.2");
+	                                                pass = false;
+	                                }
+
+	                                if (pass) {
+	                                                System.out.println("All Pass");
+	                                } else {
+	                                                System.out.println("Fail");
+	                                }
+	                }
 }
